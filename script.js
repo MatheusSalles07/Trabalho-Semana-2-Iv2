3e128becc22dbb83b1d113b3bb5fd793df35ed3a
MatheusSalles <matheus.salles@summit-bra.com>
2021-05-24 11:18:19 -0300

    concluido

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ document.querySelector('.aButton').addEventListener('click', function () {
     
     let botao = document.querySelector('.aButton')
     const headerBg = document.getElementById('header')
+    const headerTotal = document.getElementById('sections')
     const headerLink = document.querySelector('#aJS1')
     const headerLink2 = document.querySelector('#aJS2')
     const headerLink3 = document.querySelector('#aJS3')
     const headerLink4 = document.querySelector('#aJS4')
     const headerLink5 = document.querySelector('#aJS5')
     const headerLink6 = document.querySelector('#aJS6')
+    const headerLink7 = document.querySelector('#sobreMim')
+    const headerLink8 = document.querySelector('#minhasHabilidades')
+    const headerLink9 = document.querySelector('#divSection44')
+    
 
 
     if(botao.innerHTML === "Dark Mode"){
        botao.innerHTML = "Light Mode"
+       headerTotal.style.backgroundColor = "#2e388c"
        headerBg.style.backgroundColor = "purple" 
        headerLink.style.borderColor = "#FFFF"
        headerLink2.style.borderColor = "#FFFF"
@@ -25,12 +31,17 @@ document.querySelector('.aButton').addEventListener('click', function () {
        headerLink3.style.color = "#FFFF"
        headerLink4.style.color = "#FFFF"
        headerLink5.style.color = "#FFFF"
-       headerLink6.style.color = "#FFFF" 
+       headerLink6.style.color = "#FFFF"
+       headerLink7.style.color = "#FFFF"
+       headerLink8.style.color = "#FFFF"
+       headerLink9.style.color = "#FFFF"
+       botao.style.color = "#FFFF"
        
 
     }else{
         botao.innerHTML = "Dark Mode"
         headerBg.style.backgroundColor = "rgb(245, 145, 78)"
+        headerTotal.style.backgroundColor = "rgb(122, 195, 255)"
         headerLink.style.borderColor = "black"
         headerLink2.style.borderColor = "black"
         headerLink3.style.borderColor = "black"
@@ -44,5 +55,9 @@ document.querySelector('.aButton').addEventListener('click', function () {
         headerLink4.style.color = "black" 
         headerLink5.style.color = "black" 
         headerLink6.style.color = "black" 
+        headerLink7.style.color = "black"
+        headerLink8.style.color = "black"
+        headerLink9.style.color = "black"
+        botao.style.color = "black"
     }
 })
